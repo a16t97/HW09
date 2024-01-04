737f8305e70656c758e5938d472ec6e5b1febad2
Anna Tarasenko <anna.tarasenko.qa@gmail.com>
2024-01-04 14:15:28 +0200

    hw09

---
+++ src/com/hw10/exam/garage/transport/Bus.java
@@ -0,0 +1,20 @@
+package com.hw10.exam.garage.transport;
+
+import com.hw10.exam.garage.LandTrans;
+import com.hw10.exam.garage.list.type;
+
+public class Bus extends LandTrans {
+    private type type;
+
+    public Bus(int id, String name, int capacity, int yearEnding, boolean isAvailable) {
+        super(id, name, capacity, yearEnding, isAvailable);
+        this.type = type.bus;
+    }
+
+    @Override
+    public String toString() {
+        return "com.hw10.exam.garage.Bus{" +
+                "com.hw10.exam.garage.list.type=" + type +
+                "} " + super.toString();
+    }
+}
---
+++ src/com/hw10/exam/garage/transport/Car.java
@@ -0,0 +1,20 @@
+package com.hw10.exam.garage.transport;
+
+import com.hw10.exam.garage.LandTrans;
+import com.hw10.exam.garage.list.type;
+
+public class Car extends LandTrans {
+    private type type;
+
+    public Car(int id, String name, int capacity, int yearEnding, boolean isAvailable) {
+        super(id, name, capacity, yearEnding, isAvailable);
+        this.type = type.car;
+    }
+
+    @Override
+    public String toString() {
+        return "com.hw10.exam.garage.Car{" +
+                "com.hw10.exam.garage.list.type=" + type +
+                "} " + super.toString();
+    }
+}
